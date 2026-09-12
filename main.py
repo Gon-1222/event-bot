866bd493898cbdead14f3b8dae9ebc3c24cda9d0
Hajime Miyasaka <caramelpopsoft@gmail.com>
2026-09-12 22:12:33 +0900

    add list of events

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from flask import Flask, jsonify, request, send_from_directory,render_template
 from vercel.blob import BlobClient
 from discord_webhook import DiscordWebhook
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=".")
 blob_client = BlobClient()
 
 
